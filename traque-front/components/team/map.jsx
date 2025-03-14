c8974c752321ed360aeca7165dfd02acb5ad4a31
Mathieu Oriol <mathieu.oriol@telecom-paris.fr>
2025-03-14 15:03:35 +0100

    Fix carte qui ne charge pas quand on a pas la position

--- traque-front/components/team/map.jsx
+++ traque-front/components/team/map.jsx
@@ -84,7 +84,7 @@ export function LiveMap({ ...props }) {
 export function PlacementMap({ ...props }) {
     const { currentPosition, startingArea } = useGame();
     return (
-        <MapContainer  {...props} className='min-h-full w-full z-0' scrollWheelZoom={true}>
+        <MapContainer  {...props} className='min-h-full w-full z-0' center={[0, 0]} zoom={0} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
